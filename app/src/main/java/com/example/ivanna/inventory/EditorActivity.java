5fbb58d1a171887a3382f82c0a956ca2af1b14e8
evanca <ecsaine@gmail.com>
2018-06-19 21:52:42 +0300

    Get user input from editor and save new item into database

--- app/src/main/java/com/example/ivanna/inventory/EditorActivity.java
+++ app/src/main/java/com/example/ivanna/inventory/EditorActivity.java
@@ -1,8 +1,16 @@
 package com.example.ivanna.inventory;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.ivanna.inventory.ProductContract.ProductEntry;
 
 public class EditorActivity extends AppCompatActivity {
 
@@ -27,6 +35,71 @@ public class EditorActivity extends AppCompatActivity {
         mShelfEditText = findViewById(R.id.edit_product_shelf);
         mSupplierEditText = findViewById(R.id.edit_product_supplier);
         mPhoneEditText = findViewById(R.id.edit_product_phone);
+
+        final Button cancelButton = findViewById(R.id.button_cancel);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses Cancel button
+                Intent i = new Intent(EditorActivity.this, MainActivity.class);
+                finish();  // Kill the editor activity
+                startActivity(i);
+            }
+        });
+
+        final Button saveButton = findViewById(R.id.button_save);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses Save button
+                insertProduct();
+            }
+        });
+    }
+
+    /**
+     * Get user input from editor and save new item into database
+     */
+    private void insertProduct() {
+        // Read from input fields
+        // Use trim to eliminate leading or trailing white space
+        String nameString = mNameEditText.getText().toString().trim();
+        String modelString = mModelEditText.getText().toString().trim();
+        String priceString = mPriceEditText.getText().toString().trim();
+        double price = Double.parseDouble(priceString);
+        String quantityString = mQuantityEditText.getText().toString().trim();
+        int quantity = Integer.parseInt(quantityString);
+        String shelfString = mShelfEditText.getText().toString().trim();
+        String supplierString = mSupplierEditText.getText().toString().trim();
+        int supplierCode = Integer.parseInt(supplierString);
+        String phoneString = mPhoneEditText.getText().toString().trim();
+
+        // Create database helper
+        ProductDbHelper mDbHelper = new ProductDbHelper(this);
+
+        // Gets the database in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        // Create a ContentValues object where column names are the keys,
+        // and attributes from the editor are the values
+        ContentValues values = new ContentValues();
+        values.put(ProductEntry.COLUMN_PRODUCT_NAME, nameString);
+        values.put(ProductEntry.COLUMN_PRODUCT_MODEL, modelString);
+        values.put(ProductEntry.COLUMN_PRODUCT_PRICE, price);
+        values.put(ProductEntry.COLUMN_PRODUCT_QUANTITY, quantity);
+        values.put(ProductEntry.COLUMN_PRODUCT_SHELF, shelfString);
+        values.put(ProductEntry.COLUMN_PRODUCT_SUPPLIER, supplierCode);
+        values.put(ProductEntry.COLUMN_PRODUCT_PHONE, phoneString);
+
+        // Insert a new row for item in the database, returning the ID of that new row
+        long newRowId = db.insert(ProductEntry.TABLE_NAME, null, values);
+
+        // Show a toast message depending on whether or not the insertion was successful
+        if (newRowId == -1) {
+            // If the row ID is -1, then there was an error with insertion
+            Toast.makeText(this, "Error with saving a product. Please fill out all required fields.", Toast.LENGTH_SHORT).show();
+        } else {
+            // Otherwise, the insertion was successful and we can display a toast with the row ID
+            Toast.makeText(this, "Product saved with row id: " + newRowId, Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
